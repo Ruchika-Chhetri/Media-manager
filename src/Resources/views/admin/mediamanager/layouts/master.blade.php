73ca20b66e28ae8bf84c62922357f1652b35af73
ruchika <xi.palmate@gmail.com>
2022-02-22 18:27:52 +0530

    Media manager fix for bagisto

--- src/Resources/views/admin/mediamanager/layouts/master.blade.php
+++ src/Resources/views/admin/mediamanager/layouts/master.blade.php
@@ -3,7 +3,7 @@
 @section('content-wrapper')
     <div class="inner-section">
     
-        @include ('admin::layouts.nav-aside')
+        @include ('admin::layouts.nav-left')
 
         <div class="content-wrapper">
 
